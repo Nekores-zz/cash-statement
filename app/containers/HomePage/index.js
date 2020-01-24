6f483ed4e1ffa763cd81819517454a1ae6f3c00f
nekores <sajidxullah@gmail.com>
2020-01-24 06:17:22 +0500

    integration test

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -61,10 +61,7 @@ export function HomePage({
     <article>
       <Helmet>
         <title>Home Page</title>
-        <meta
-          name="description"
-          content="A React.js Boilerplate application homepage"
-        />
+        <meta name="description" content="Cash Statement" />
       </Helmet>
       <div>
         <CenteredSection>
